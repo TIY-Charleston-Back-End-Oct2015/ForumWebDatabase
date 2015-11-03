82bb5008d96257a80d6b18af90106a8d8bf141ca
oakes <zsoakes@gmail.com>
2015-11-03 12:11:40 -0500

    Use database instead of in-memory data structures

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -80,24 +80,13 @@ public class Main {
         Connection conn = DriverManager.getConnection("jdbc:h2:./main");
         createTables(conn);
 
-	    HashMap<String, User> users = new HashMap();
-        ArrayList<Message> messages = new ArrayList();
-
-        addTestUsers(users);
-        addTestMessages(messages);
-
         Spark.get(
                 "/",
                 ((request, response) -> {
                     Session session = request.session();
                     String username = session.attribute("username");
 
-                    ArrayList<Message> threads = new ArrayList();
-                    for (Message message : messages) {
-                        if (message.replyId == -1) {
-                            threads.add(message);
-                        }
-                    }
+                    ArrayList<Message> threads = selectReplies(conn, -1);
 
                     HashMap m = new HashMap();
                     m.put("threads", threads);
@@ -119,16 +108,11 @@ public class Main {
                     String id = request.queryParams("id");
                     try {
                         int idNum = Integer.valueOf(id);
-                        Message message = messages.get(idNum);
+                        Message message = selectMessage(conn, idNum);
                         m.put("message", message);
                         m.put("replyId", message.id);
 
-                        ArrayList<Message> replies = new ArrayList();
-                        for (Message msg : messages) {
-                            if (msg.replyId == message.id) {
-                                replies.add(msg);
-                            }
-                        }
+                        ArrayList<Message> replies = selectReplies(conn, message.id);
                         m.put("replies", replies);
                     } catch (Exception e) {
 
@@ -148,11 +132,9 @@ public class Main {
                         Spark.halt(403);
                     }
 
-                    User user = users.get(username);
+                    User user = selectUser(conn, username);
                     if (user == null) {
-                        user = new User();
-                        user.password = password;
-                        users.put(username, user);
+                        insertUser(conn, username, password);
                     }
                     else if (!password.equals(user.password)) {
                         Spark.halt(403);
@@ -179,8 +161,8 @@ public class Main {
                     String text = request.queryParams("text");
                     try {
                         int replyIdNum = Integer.valueOf(replyId);
-                        Message message = new Message(messages.size(), replyIdNum, username, text);
-                        messages.add(message);
+                        User me = selectUser(conn, username);
+                        insertMessage(conn, me.id, replyIdNum, text);
                     } catch (Exception e) {
 
                     }
@@ -189,18 +171,5 @@ public class Main {
                     return "";
                 })
         );
-    }
-
-    static void addTestUsers(HashMap<String, User> users) {
-        users.put("Alice", new User());
-        users.put("Bob", new User());
-        users.put("Charlie", new User());
-    }
-
-    static void addTestMessages(ArrayList<Message> messages) {
-        messages.add(new Message(0, -1, "Alice", "This is a thread!"));
-        messages.add(new Message(1, -1, "Bob", "This is a thread!"));
-        messages.add(new Message(2, 0, "Charlie", "Cool thread, Alice."));
-        messages.add(new Message(3, 2, "Alice", "Thanks"));
     }
 }
